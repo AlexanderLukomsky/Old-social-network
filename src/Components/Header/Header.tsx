ab5a323948f320a25c98b2466b78e63a16be133b
Alexander Lukomsky <lukomsky.alexander@gmail.com>
2021-07-19 17:31:10 +0300

    add components: header, technologies

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/">News Feed</a>
+      </li>
+      <li>
+        <a href="/">Messages</a>
+      </li>
+    </ul>
+  );
+}
+export default Header
